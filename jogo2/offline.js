3d3ec1ea9f62ac0244e01268c3c480f192f913e0
matheusliou <78481846+matheusliou@users.noreply.github.com>
2021-09-11 00:29:05 -0300

    Add files via upload

---
+++ jogo2/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1628714392,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/chão-sheet0.png",
+		"images/personagem-sheet0.png",
+		"images/comida-sheet0.png",
+		"images/nuvens.png",
+		"images/céu.png",
+		"images/moeda-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
