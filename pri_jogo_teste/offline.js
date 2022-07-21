2157b3d995e98d6b5b1f3d05073677577ac9648c
matheusliou <78481846+matheusliou@users.noreply.github.com>
2022-07-21 19:07:03 -0300

    jogo pri

---
+++ pri_jogo_teste/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1658440964,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/chão.png",
+		"images/pri-sheet0.png",
+		"images/pular-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/par-sheet0.png",
+		"images/tiledbackground.png",
+		"images/chão2.png",
+		"images/botão-sheet0.png"
+	]
+}
